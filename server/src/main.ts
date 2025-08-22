518fc7b7f84f5b46c7440a92f006b4d32d05a9ec
Asad Sohail <asadsohailabid@gmail.com>
2025-08-22 20:13:20 +0500

    added apollo server with gql

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,28 +1,12 @@
+import { startStandaloneServer } from '@apollo/server/standalone';
+import gqlServer from './gql/schema';
 import config from './config/config';
-import app from './app';
 
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-async function main() {
-  const post = await prisma.post.update({
-    where: { id: 1 },
-    data: { published: true },
-  })
-  console.log(post)
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
-
-app.listen(config.port, '0.0.0.0', () => {
-  console.log(`Server running on port ${config.port}`);
+//  1. creates an Express app
+//  2. installs ApolloServer instance as middleware
+//  3. prepares app to handle incoming requests
+startStandaloneServer(gqlServer, {
+  listen: { port: config.port },
+}).then(({ url }) => {
+  console.log(`🚀  Server ready at: ${url}`);
 });
